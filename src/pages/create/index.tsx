62abe188520a8cfd123cc5a9e4d05964054bbe35
Vincent Lemesle <vincent.lemesle@epitech.eu>
2022-12-02 20:09:44 +0100

    edit: create post

--- src/pages/create/index.tsx
+++ src/pages/create/index.tsx
@@ -1,14 +1,20 @@
+import { Text } from "@nextui-org/react";
+import { useEffect, useState } from "react";
+
 import Layout from "../../components/layout";
-import Article from "../../components/article";
-import ArticleList from "../../components/articlesList";
-import {useState} from "react";
-import {Text} from "@nextui-org/react";
 
 const Index = () => {
-  const [title, setTitle] = useState('');
   const [desc, setDesc] = useState('');
+  const [title, setTitle] = useState('');
+  const [pos, setPos] = useState(undefined);
 
-  console.log(desc);
+  useEffect(() => {
+    if (!pos) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setPos({ lat: position.coords.latitude, lng: position.coords.longitude });
+      });
+    }
+  });
 
   return (
     <Layout menuIndex={-1}>
@@ -29,7 +35,12 @@ const Index = () => {
           style={{ width: 400, height: 100 }}
           onChange={(e) => setDesc(e.target.value)}
         />
-        <button style={{ backgroundColor: '#380038', cursor: 'pointer', width: 150, height: 40, borderRadius: 5, marginTop: 60 }}>
+        {!pos && (
+          <Text h6 color="#480048" style={{ marginTop: 60 }}>
+            ce post ne sera pas lié à une localisation (vous n'avez pas accepté de la partager)
+          </Text>
+        )}
+        <button style={{ backgroundColor: '#380038', cursor: 'pointer', width: 150, height: 40, borderRadius: 5, marginTop: pos ? 60 : 5 }}>
           <Text h4 color="#F2F2F2" style={{ padding: 0, margin: 0 }}>
             POSTER
           </Text>
