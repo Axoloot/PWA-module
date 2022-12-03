3659f303baac29c67a6b22f92716c70e4ae8d13b
Jérémy Groizard <jeremy.groizard@epitech.eu>
2022-12-03 04:19:35 +0100

    Impl notification click

--- src/worker/index.js
+++ src/worker/index.js
@@ -12,3 +12,21 @@ self.addEventListener("push", (event) => {
   );
   channel.postMessage(data);
 });
+
+self.addEventListener('notificationclick', (event) => {
+  const url = 'http://localhost:3001/home';
+
+  event.waitUntil(
+    clients.matchAll({type: 'window'}).then( windowClients => {
+      for (var i = 0; i < windowClients.length; i++) {
+        var client = windowClients[i];
+        if (client.url === url && 'focus' in client) {
+          return client.focus();
+        }
+      }
+      if (clients.openWindow) {
+        return clients.openWindow(url);
+      }
+    })
+  );
+});
